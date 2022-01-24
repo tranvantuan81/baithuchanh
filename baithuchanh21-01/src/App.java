7e0683b0d97d410778154b6fb9bc9d9615dfe090
tranvantuan81 <tuanthuan8102@gmail.com>
2022-01-24 19:58:57 +0700

    commit note

--- baithuchanh21-01/src/App.java
+++ baithuchanh21-01/src/App.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
-public class App {
-    public static void main(String[] args) {
+public class App2 {
+    public static void main(String []args) {
+        double a1, b1, c1, a2, b2, c2;
+        double D, Dx, Dy, x, y;
         Scanner sc = new Scanner(System.in);
-        double a, b, c;       
-        double x1, x2;       
-        System.out.print("Nhap he so a = ");
-        a = sc.nextDouble();
-        System.out.print("Nhap he so b = ");
-        b = sc.nextDouble();
-        System.out.print("Nhap he so c = ");
-        c = sc.nextDouble();
-        double delta = b*b - 4*a*c;
-        if(delta > 0) {
-            x1 = (float) ((-b + Math.sqrt(delta)) / (2*a));
-            x2 = (float) ((-b - Math.sqrt(delta)) / (2*a));
-            System.out.println("PT co 2 nghiem: " + "x1 = " + x1 + " và x2 = " + x2);
-        } else if(delta == 0) {
-            x1 = (-b / (2 * a));
-            System.out.println("PT co nghiem kep: " + "x1 = x2 = " + x1);
-        } else 
-            System.out.println("PT vo nghiem");
+        System.out.println("a1*x + b1*y = c1");
+        System.out.println("a2*x + b2*y = c2");
+        System.out.print("Nhap a1: ");
+        a1 = sc.nextDouble();
+        System.out.print("Nhap b1: ");
+        b1 = sc.nextDouble();
+        System.out.print("Nhap c1: ");
+        c1 = sc.nextDouble();
+        System.out.print("Nhap a2: ");
+        a2 = sc.nextDouble();
+        System.out.print("Nhap b2: ");
+        b2 = sc.nextDouble();
+        System.out.print("Nhap c2: ");
+        c2 = sc.nextDouble();
+        D = a1 * b2 - a2 * b1;
+        Dx = c1 * b2 - c2 * b1;
+        Dy = a1 * c2 - a2 * c1;
+        if (D == 0) {
+            if (Dx + Dy == 0)
+                System.out.println("He phuong trinh co vo so nghiem");
+            else
+                System.out.println("He phuong trinh vo nghiem");
+        }
+        else {
+            x = Dx / D;
+            y = Dy / D;
+            System.out.print("He phuong trinh co nghiem x = " + x + " y = " + y);
+        }
         sc.close();
-    }    
+    }
 }
