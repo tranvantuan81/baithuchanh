a35289fab4802ff3a1a0e91ee4db8898afdaf6eb
tranvantuan81 <tuanthuan8102@gmail.com>
2022-02-19 16:15:16 +0700

    commit.note

--- baithuchanh1802/src/App30.java
+++ baithuchanh1802/src/App30.java
@@ -1,8 +1,7 @@
 public static void main(String[] args) {
     int n, soDu, tong = 0;
     Scanner scanner = new Scanner(System.in);
-         
-    System.out.println("Nhập vào số nguyên dương bất kỳ: ");
+          System.out.println("nhap so nguyen bat ky: ");
     n = scanner.nextInt();
          
     while (n > 0) {
---
+++ baithuchanh1802/src/App51.java
@@ -0,0 +1,11 @@
+public class App51 {
+    public static void main (String[] args) {
+        int so =0;
+        for (so=0; so<5;so++){
+            System.out.println("so");
+        }
+    }
+
+}
+    
+
